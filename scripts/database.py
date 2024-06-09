822352405b7c6126bf30ec1eea5c602eb8ad51a6
Vicente Caragol <55118496+VicenteCaragol@users.noreply.github.com>
2024-06-08 22:26:40 -0400

    made decent typechart

--- scripts/database.py
+++ scripts/database.py
@@ -31,79 +31,44 @@ type_map_inverted = ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fig
 
 
 def build_typechart():
-    typechart= np.array([
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #Normal
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #Fire
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #Water
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #Electric
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    ], dtype=np.float32)
-    #Normal
-    typechart[type_map['normal'],type_map['rock']]=0.5
-    typechart[type_map['normal'],type_map['ghost']]=0
-    typechart[type_map['normal'],type_map['steel']]=0.5
-    #Fire
-    typechart[type_map['fire'],type_map['fire']]=0.5
-    typechart[type_map['fire'],type_map['water']]=0.5
-    typechart[type_map['fire'],type_map['grass']]=2
-    typechart[type_map['fire'],type_map['ice']]=2
-    typechart[type_map['fire'],type_map['bug']]=2
-    typechart[type_map['fire'],type_map['rock']]=0.5
-    typechart[type_map['fire'],type_map['dragon']]=0.5
-    typechart[type_map['fire'],type_map['steel']]=2
-    #Water
-    typechart[type_map['water'],type_map['fire']]=2
-    typechart[type_map['water'],type_map['water']]=0.5
-    typechart[type_map['water'],type_map['grass']]=0.5
-    typechart[type_map['water'],type_map['ground']]=2
-    typechart[type_map['water'],type_map['rock']]=2
-    typechart[type_map['water'],type_map['dragon']]=0.5
-    #Electric
-    typechart[type_map['electric'],type_map['water']]=2
-    typechart[type_map['electric'],type_map['electric']]=0.5
-    typechart[type_map['electric'],type_map['grass']]=0.5
-    typechart[type_map['electric'],type_map['ground']]=0
-    typechart[type_map['electric'],type_map['flying']]=2
-    typechart[type_map['electric'],type_map['dragon']]=0.5
-    #Grass
-    for type,mult in [['fire',0.5],['water',2],['grass',0.5],['poison',0.5],['ground',2],['bug',0.5],['rock',2],['dragon',0.5],['steel',0.5]]:
-        typechart[type_map['grass'],type_map[type]]=mult
-    #Ice
-    for type,mult in [['fire',0.5],['water',0.5],['grass',2],['ice',0.5],['ground',2],['flying',2],['dragon',2],['steel',0.5]]:
-        typechart[type_map['ice'],type_map[type]]=mult
-    #Fighting
-    for type,mult in [['normal',2],['ice',2],['poison',0.5],['flying',0.5],['psychic',0.5],['bug',0.5],['rock',2],['ghost',0],['dark',2],['steel',2],['fairy',0.5]]:
-        typechart[type_map['fighting'],type_map[type]]=mult
-    #Poison
-    typechart[type_map['poison'],type_map['grass']]=2
-    typechart[type_map['poison'],type_map['poison']]=0.5
-    typechart[type_map['poison'],type_map['ground']]=0.5
-    typechart[type_map['poison'],type_map['rock']]=0.5
-    typechart[type_map['poison'],type_map['ghost']]=0.5
-    typechart[type_map['poison'],type_map['steel']]=0
-    typechart[type_map['poison'],type_map['fairy']]=2
-    #Ground
-    typechart[type_map['ground'],type_map['fire']]=2
-    typechart[type_map['ground'],type_map['electric']]=2
-    typechart[type_map['ground'],type_map['grass']]=0.5
-    typechart[type_map['ground'],type_map['poison']]=2
-    typechart[type_map['ground'],type_map['flying']]=0
-    typechart[type_map['ground'],type_map['bug']]=0.5
-    typechart[type_map['ground'],type_map['rock']]=2
-    typechart[type_map['ground'],type_map['steel']]=2
+    typechart= np.array([[1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 0.5,
+        0. , 1. , 1. , 0.5, 1. ],#Normal
+       [1. , 0.5, 0.5, 1. , 2. , 2. , 1. , 1. , 1. , 1. , 1. , 2. , 0.5,
+        1. , 0.5, 1. , 2. , 1. ],#Fire
+       [1. , 2. , 0.5, 1. , 0.5, 1. , 1. , 1. , 2. , 1. , 1. , 1. , 2. ,
+        1. , 0.5, 1. , 1. , 1. ],#Water
+       [1. , 1. , 2. , 0.5, 0.5, 1. , 1. , 1. , 0. , 2. , 1. , 1. , 1. ,
+        1. , 0.5, 1. , 1. , 1. ],#Grass
+       [1. , 0.5, 2. , 1. , 0.5, 1. , 1. , 0.5, 2. , 1. , 1. , 0.5, 2. ,
+        1. , 0.5, 1. , 0.5, 1. ],#Electric
+       [1. , 0.5, 0.5, 1. , 2. , 0.5, 1. , 1. , 2. , 2. , 1. , 1. , 1. ,
+        1. , 2. , 1. , 0.5, 1. ],#Ice
+       [2. , 1. , 1. , 1. , 1. , 2. , 1. , 0.5, 1. , 0.5, 0.5, 0.5, 2. ,
+        0. , 1. , 2. , 2. , 0.5],#Fighting
+       [1. , 1. , 1. , 1. , 2. , 1. , 1. , 0.5, 0.5, 1. , 1. , 1. , 0.5,
+        0.5, 1. , 1. , 0. , 2. ],#Poison
+       [1. , 2. , 1. , 2. , 0.5, 1. , 1. , 2. , 1. , 0. , 1. , 0.5, 2. ,
+        1. , 1. , 1. , 2. , 1. ],#Ground
+       [1. , 1. , 1. , 0.5, 2. , 1. , 2. , 1. , 1. , 1. , 1. , 2. , 0.5,
+        1. , 1. , 1. , 0.5, 1. ],#Flying
+       [1. , 1. , 1. , 1. , 1. , 1. , 2. , 2. , 1. , 1. , 1. , 0.5, 1. ,
+        1. , 1. , 0. , 0.5, 1. ],#Psychic
+       [1. , 0.5, 1. , 1. , 2. , 1. , 0.5, 0.5, 1. , 0.5, 2. , 1. , 1. ,
+        0.5, 1. , 2. , 0.5, 0.5],#Bug
+       [1. , 2. , 1. , 1. , 1. , 2. , 0.5, 1. , 0.5, 2. , 1. , 2. , 1. ,
+        1. , 1. , 1. , 0.5, 1. ],#Rock
+       [0. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 2. , 1. , 1. ,
+        2. , 1. , 0.5, 1. , 1. ],#Ghost
+       [1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. , 1. ,
+        1. , 2. , 1. , 0.5, 0. ],#Dragon
+       [1. , 1. , 1. , 1. , 1. , 1. , 0.5, 1. , 1. , 1. , 2. , 1. , 1. ,
+        2. , 1. , 0.5, 1. , 0.5],#Dark
+       [1. , 0.5, 0.5, 0.5, 1. , 2. , 1. , 1. , 1. , 1. , 1. , 1. , 2. ,
+        1. , 1. , 1. , 0.5, 2. ],#Steel
+       [1. , 0.5, 1. , 1. , 1. , 1. , 2. , 0.5, 1. , 1. , 1. , 1. , 1. , #Fairy
+        1. , 2. , 2. , 0.5, 1. ]], dtype=np.float32)
+
+    #typechart[type_map['ground'],type_map['steel']]=2
 
 
     return typechart
